e138cc6513efd52e490d8d27477176bc6b574a17
Gilles Doge <gilles@antistatique.net>
2013-06-30 21:07:00 +0200

    Add file header

--- Tests/Listener/CommandListenerTest.php
+++ Tests/Listener/CommandListenerTest.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Sonata package.
+ * This file is part of the Sonata project.
  *
- * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
