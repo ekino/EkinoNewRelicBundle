97a3ffb4f241d9a19a25efac61fc67c9528aaea5
Jérôme Tamarelle <jerome@tamarelle.net>
2013-09-07 01:27:48 +0200

    Add command support for Silex

--- Silex/EkinoNewRelicServiceProvider.php
+++ Silex/EkinoNewRelicServiceProvider.php
@@ -1,22 +1,36 @@
 <?php
 
+/*
+ * This file is part of Ekino New Relic bundle.
+ *
+ * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ekino\Bundle\NewRelicBundle\Silex;
 
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Console\ConsoleEvents;
 
 use Ekino\Bundle\NewRelicBundle\NewRelic\NewRelic;
 use Ekino\Bundle\NewRelicBundle\NewRelic\NewRelicInteractor;
 use Ekino\Bundle\NewRelicBundle\NewRelic\LoggingInteractorDecorator;
 use Ekino\Bundle\NewRelicBundle\Listener\RequestListener;
-use Ekino\Bundle\NewRelicBundle\Listener\ExceptionListener;
 use Ekino\Bundle\NewRelicBundle\Listener\ResponseListener;
+use Ekino\Bundle\NewRelicBundle\Listener\ExceptionListener;
+use Ekino\Bundle\NewRelicBundle\Listener\CommandListener;
 use Ekino\Bundle\NewRelicBundle\TransactionNamingStrategy\RouteNamingStrategy;
+use Ekino\Bundle\NewRelicBundle\Command\NotifyDeploymentCommand;
 
 /**
- * @author  Jérôme TAMARELLE <jerome@tamarelle.net>
+ * Service Provider for Silex
+ *
+ * @author Jérôme TAMARELLE <jerome@tamarelle.net>
  */
 class EkinoNewRelicServiceProvider implements ServiceProviderInterface
 {
@@ -27,7 +41,7 @@ class EkinoNewRelicServiceProvider implements ServiceProviderInterface
         $app['new_relic.api_key'] = null;
         $app['new_relic.logging'] = false;
         $app['new_relic.log_exceptions'] = false;
-        $app['new_relic.log_commands'] = false;
+        $app['new_relic.log_commands'] = class_exists('Symfony\\Component\\Console\\ConsoleEvents');
 
         $app['new_relic'] = $app->share(function ($app) {
             return new NewRelic($app['new_relic.application_name'], $app['new_relic.api_key']);
@@ -39,6 +53,7 @@ class EkinoNewRelicServiceProvider implements ServiceProviderInterface
             return new RouteNamingStrategy();
         });
 
+        // Interactor
         $app['new_relic.interactor'] = $app->share(function ($app) {
             if ($app['new_relic.logging']) {
                 return $app['new_relic.interactor.logger'];
@@ -46,23 +61,30 @@ class EkinoNewRelicServiceProvider implements ServiceProviderInterface
 
             return $app['new_relic.interactor.real'];
         });
-
         $app['new_relic.interactor.real'] = $app->share(function ($app) {
             return new NewRelicInteractor();
         });
-
         $app['new_relic.interactor.logger'] = $app->share(function ($app) {
             return new LoggingInteractorDecorator($app['new_relic.interactor.real'], isset($app['logger']) ? $app['logger'] : null);
         });
 
+        // Listeners
         $app['new_relic.request_listener'] = $app->share(function ($app) {
             return new RequestListener($app['new_relic'], $app['new_relic.interactor'], array(), array(), $app['new_relic.transaction_naming_strategy']);
         });
         $app['new_relic.exception_listener'] = $app->share(function ($app) {
-            return new ResponseListener($app['new_relic.interactor']);
+            return new ExceptionListener($app['new_relic.interactor']);
         });
         $app['new_relic.response_listener'] = $app->share(function ($app) {
             return new ResponseListener($app['new_relic'], $app['new_relic.interactor'], false);
+        });
+        $app['new_relic.console_listener'] = $app->share(function ($app) {
+            return new CommandListener($app['new_relic.interactor']);
+        });
+
+        // Optional command
+        $app['new_relic.command.notify'] = $app->share(function ($app) {
+            return new NotifyDeploymentCommand($app['new_relic']);
         });
     }
 
@@ -78,6 +100,14 @@ class EkinoNewRelicServiceProvider implements ServiceProviderInterface
 
         if ($app['new_relic.log_exceptions']) {
             $app['dispatcher']->addListener(KernelEvents::EXCEPTION, array($app['new_relic.exception_listener'], 'onKernelException'), 0);
+        }
+
+        if ($app['new_relic.log_commands']) {
+            $app['dispatcher']->addListener(ConsoleEvents::COMMAND, array($app['new_relic.console_listener'], 'onConsoleCommand'), 0);
+        }
+
+        if ($app['new_relic.log_exceptions'] && $app['new_relic.log_commands']) {
+            $app['dispatcher']->addListener(ConsoleEvents::EXCEPTION, array($app['new_relic.exception_listener'], 'onKernelException'), 0);
         }
     }
 }
--- Tests/Silex/EkinoNewRelicServiceProviderTest.php
+++ Tests/Silex/EkinoNewRelicServiceProviderTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Ekino New Relic bundle.
+ *
+ * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ekino\Bundle\NewRelicBundle\Tests\Silex;
 
 use Silex\Application;
@@ -7,9 +16,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Ekino\Bundle\NewRelicBundle\Silex\EkinoNewRelicServiceProvider;
 
+/**
+ * Validate
+ *
+ * @author Jérôme Tamarelle <jerome@tamarelle.net>
+ */
 class EkinoNewRelicServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testListenerCalled()
+    public function testRequestListener()
     {
         $app = $this->createApplication();
 
@@ -30,7 +44,50 @@ class EkinoNewRelicServiceProviderTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(200, $response->getStatusCode());
     }
 
-    public function createApplication()
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage Test Exception
+     */
+    public function testExceptionListener()
+    {
+        $app = $this->createApplication();
+
+        $app['new_relic.log_exceptions'] = true;
+
+        $app['new_relic.interactor.real']
+            ->expects($this->once())
+            ->method('noticeException')
+        ;
+
+        $response = $app->handle(Request::create('/error'));
+    }
+
+    /**
+     * @expectedException        RuntimeException
+     * @expectedExceptionMessage Test Exception
+     */
+    public function testExceptionListenerDisabledByDefault()
+    {
+        $app = $this->createApplication();
+
+        $this->assertFalse($app['new_relic.log_exceptions']);
+
+        $app['new_relic.interactor.real']
+            ->expects($this->never())
+            ->method('noticeException')
+        ;
+
+        $response = $app->handle(Request::create('/error'));
+    }
+
+    public function testCommandRegistered()
+    {
+        $app = $this->createApplication();
+
+        $this->assertInstanceOf('Symfony\\Component\\Console\\Command\\Command', $app['new_relic.command.notify']);
+    }
+
+    private function createApplication()
     {
         $app = new Application();
         unset($app['exception_handler']);
@@ -46,7 +103,7 @@ class EkinoNewRelicServiceProviderTest extends \PHPUnit_Framework_TestCase
         })->bind('my_page');
 
         $app->get('/error', function () {
-            throw new \RuntimeException('test');
+            throw new \RuntimeException('Test Exception');
         })->bind('my_error');
 
         return $app;
