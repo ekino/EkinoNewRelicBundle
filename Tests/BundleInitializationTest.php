4d6be2bfa51a48ee8b97a8a94c4921c51c6d7e89
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-03-17 16:27:58 +0100

    Improve testing (#138)
    * Removed legacy files
    
    * Added smoke test
    
    * Added tests on extension
    
    * cs
    
    * Force PHPUnit version
    
    * minor

---
+++ Tests/BundleInitializationTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ekino New Relic bundle.
+ *
+ * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ekino\Bundle\NewRelicBundle\Tests;
+
+use Ekino\Bundle\NewRelicBundle\EkinoNewRelicBundle;
+use Ekino\Bundle\NewRelicBundle\NewRelic\BlackholeInteractor;
+use Ekino\Bundle\NewRelicBundle\NewRelic\NewRelicInteractor;
+use Nyholm\BundleTest\BaseBundleTestCase;
+use Nyholm\BundleTest\CompilerPass\PublicServicePass;
+
+/**
+ * Smoke test to see if the bundle can run.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class BundleInitializationTest extends BaseBundleTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // Make services public that have an idea that matches a regex
+        $this->addCompilerPass(new PublicServicePass('|ekino.new_relic.interactor.*|'));
+    }
+
+    protected function getBundleClass()
+    {
+        return EkinoNewRelicBundle::class;
+    }
+
+    public function testInitBundle()
+    {
+        // Boot the kernel.
+        $this->bootKernel();
+
+        // Get the container
+        $container = $this->getContainer();
+
+        $services = [
+            'ekino.new_relic.interactor.real' => NewRelicInteractor::class,
+            'ekino.new_relic.interactor.blackhole' => BlackholeInteractor::class,
+        ];
+
+        // Test if you services exists
+        foreach ($services as $id => $class) {
+            $this->assertTrue($container->has($id));
+            $service = $container->get($id);
+            $this->assertInstanceOf($class, $service);
+        }
+    }
+}
---
+++ Tests/DependencyInjection/EkinoNewRelicExtensionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Ekino New Relic bundle.
+ *
+ * (c) Ekino - Thomas Rabaix <thomas.rabaix@ekino.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ekino\Bundle\NewRelicBundle\Tests\DependencyInjection;
+
+use Ekino\Bundle\NewRelicBundle\DependencyInjection\EkinoNewRelicExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+
+class EkinoNewRelicExtensionTest extends AbstractExtensionTestCase
+{
+    protected function getContainerExtensions()
+    {
+        return [new EkinoNewRelicExtension()];
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->setParameter('kernel.bundles', []);
+    }
+
+    public function testDefaultConfiguration()
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasService('ekino.new_relic.twig.new_relic_extension');
+        $this->assertContainerBuilderHasService('ekino.new_relic.command_listener');
+        $this->assertContainerBuilderNotHasService('ekino.new_relic.exception_listener');
+    }
+
+    public function testAlternativeConfiguration()
+    {
+        $this->load([
+            'log_exceptions' => true,
+            'log_commands' => false,
+            'twig' => false,
+        ]);
+
+        $this->assertContainerBuilderNotHasService('ekino.new_relic.twig.new_relic_extension');
+        $this->assertContainerBuilderNotHasService('ekino.new_relic.command_listener');
+        $this->assertContainerBuilderHasService('ekino.new_relic.exception_listener');
+    }
+}
