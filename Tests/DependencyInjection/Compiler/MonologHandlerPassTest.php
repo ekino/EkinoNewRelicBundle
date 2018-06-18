c1af63155577469b54179f22a75ea6dc2728d854
Jérémy Derussé <jeremy@derusse.com>
2018-06-18 11:31:02 +0200

    Fine tuning for Monolog Configuration

--- Tests/DependencyInjection/Compiler/MonologHandlerPassTest.php
+++ Tests/DependencyInjection/Compiler/MonologHandlerPassTest.php
@@ -27,13 +27,36 @@ class MonologHandlerPassTest extends AbstractCompilerPassTestCase
 
     public function testProcessChannel()
     {
-        $this->container->setParameter('ekino.new_relic.monolog.channels', ['app', 'foo']);
-        $this->registerService('monolog.logger', \Monolog\Logger::class);
-        $this->registerService('monolog.logger.foo', \Monolog\Logger::class);
+        $this->container->setParameter('ekino.new_relic.monolog.channels', ['type' => 'inclusive', 'elements' => ['app', 'foo']]);
+        $this->registerService('monolog.logger', \Monolog\Logger::class)->setArgument(0, 'app');
+        $this->registerService('monolog.logger.foo', \Monolog\Logger::class)->setArgument(0, 'foo');
 
         $this->compile();
 
         $this->assertContainerBuilderHasServiceDefinitionWithMethodCall('monolog.logger', 'pushHandler', [new Reference('ekino.new_relic.logs_handler')]);
         $this->assertContainerBuilderHasServiceDefinitionWithMethodCall('monolog.logger.foo', 'pushHandler', [new Reference('ekino.new_relic.logs_handler')]);
+    }
+
+    public function testProcessChannelAllChannels()
+    {
+        $this->container->setParameter('ekino.new_relic.monolog.channels', null);
+        $this->registerService('monolog.logger', \Monolog\Logger::class)->setArgument(0, 'app');
+        $this->registerService('monolog.logger.foo', \Monolog\Logger::class)->setArgument(0, 'foo');
+
+        $this->compile();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithMethodCall('monolog.logger', 'pushHandler', [new Reference('ekino.new_relic.logs_handler')]);
+        $this->assertContainerBuilderHasServiceDefinitionWithMethodCall('monolog.logger.foo', 'pushHandler', [new Reference('ekino.new_relic.logs_handler')]);
+    }
+
+    public function testProcessChannelExcludeChannels()
+    {
+        $this->container->setParameter('ekino.new_relic.monolog.channels', ['type' => 'exclusive', 'elements' => ['foo']]);
+        $this->registerService('monolog.logger', \Monolog\Logger::class)->setArgument(0, 'app');
+        $this->registerService('monolog.logger.foo', \Monolog\Logger::class)->setArgument(0, 'foo');
+
+        $this->compile();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithMethodCall('monolog.logger', 'pushHandler', [new Reference('ekino.new_relic.logs_handler')]);
     }
 }
