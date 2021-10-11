c328bea5f48f251a0a22dcee4a6218f4b65e1526
Gerd Rönsch <gerd.roensch@digistore24.team>
2021-10-11 09:07:46 +0200

    Fix style

--- DependencyInjection/EkinoNewRelicExtension.php
+++ DependencyInjection/EkinoNewRelicExtension.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace  Ekino\NewRelicBundle\DependencyInjection;
+namespace Ekino\NewRelicBundle\DependencyInjection;
 
 use Ekino\NewRelicBundle\Listener\CommandListener;
 use Ekino\NewRelicBundle\Listener\RequestListener;
